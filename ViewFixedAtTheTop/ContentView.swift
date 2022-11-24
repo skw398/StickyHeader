81b62a8ff874b8d66184949d92c5409053bf82f4
oshio07 <skw398@gmail.com>
2022-11-24 19:24:49 +0900

    Initial Commit

---
+++ ViewFixedAtTheTop/ContentView.swift
@@ -0,0 +1,59 @@
+//
+//  ContentView.swift
+//  ViewFixedAtTheTop
+//
+//  Created by Shigenari Oshio on 2022/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    private let fixedViewHeight: CGFloat = 100
+    
+    var body: some View {
+        NavigationView {
+            GeometryReader { scrollViewGeo in
+                ScrollView {
+                    Text("Some Content")
+                        .frame(height: 300)
+                        .frame(maxWidth: .infinity)
+                        .background(.cyan)
+                    
+                    VStack {
+                        GeometryReader { fixedViewGeo in
+                            Text("Fixed View")
+                                .font(.largeTitle)
+                                .fontWeight(.black)
+                                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                                .background(.green)
+                                .offset(
+                                    y: {
+                                        let distanceToTheTop = scrollViewGeo.frame(in: .global).minY - fixedViewGeo.frame(in: .global).minY
+                                        return distanceToTheTop > 0 ? distanceToTheTop : 0
+                                    }()
+                                )
+                        }
+                        .frame(height: fixedViewHeight)
+                        .zIndex(1)
+                        
+                        VStack(spacing: 0) {
+                            ForEach(0..<10) { i in
+                                Text("Some Long Content")
+                                    .frame(height: 100)
+                                    .frame(maxWidth: .infinity)
+                                    .background(.yellow)
+                            }
+                        }
+                    }
+                }
+                .navigationTitle("Title")
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ViewFixedAtTheTop/ViewFixedAtTheTopApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewFixedAtTheTopApp.swift
+//  ViewFixedAtTheTop
+//
+//  Created by Shigenari Oshio on 2022/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ViewFixedAtTheTopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
